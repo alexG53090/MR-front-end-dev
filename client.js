84c658108feb43fcb3dfd01ddfe5b0d6cb0b7e2d
Alex Bennett <alexandergraybennett@gmail.com>
2016-01-08 17:12:31 -0700

    setting up modules for client js

---
+++ client.js
@@ -0,0 +1,9 @@
+var timeline = require('./js/timeline');
+var blogpost = require('./js/blogpost');
+var profile = require('./js/profile');
+var blogcreate = require('./js/blogcreate');
+
+timeline();
+blogpost();
+blogcreate();
+profile();
