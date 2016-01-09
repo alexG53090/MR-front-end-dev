29335e22aef6e4a1f2b1a3cfba39a820227e97bd
Alex <alexandergraybennett@gmail.com>
2016-01-08 23:27:30 -0700

    Delete client.js

--- client.js
+++
@@ -1,9 +0,0 @@
-var timeline = require('./js/timeline');
-var blogpost = require('./js/blogpost');
-var profile = require('./js/profile');
-var blogcreate = require('./js/blogcreate');
-
-timeline();
-blogpost();
-blogcreate();
-profile();
