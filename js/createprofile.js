6285b116b717a11f9ac96202ead17f0bc035e21e
Alex Bennett <alexandergraybennett@gmail.com>
2016-01-08 17:28:21 -0700

    adding logic files for all app views

---
+++ js/createprofile.js
@@ -0,0 +1,6 @@
+var createProfile = function(){
+  console.log("I'm the js for create profile view")
+}
+
+
+module.exports = createProfile;
---
+++ js/filter.js
@@ -0,0 +1,6 @@
+var filter = function(){
+  console.log("I'm the js for the filter funciton")
+}
+
+
+module.exports = filter;
